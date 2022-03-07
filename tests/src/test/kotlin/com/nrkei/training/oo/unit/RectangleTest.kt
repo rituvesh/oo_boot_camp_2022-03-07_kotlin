4a83dbef07c2a6764d6c2512862e353f6d6f2706
Fred George <fredgeorge@acm.org>
2022-03-07 13:21:36 +0100

    Fred: Rectangle with area, perimeter, and range checking

--- tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
@@ -9,11 +9,28 @@ package com.nrkei.training.oo.unit
 import com.nrkei.training.oo.rectangle.Rectangle
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 // Ensures Rectangle operates correctly
 internal class RectangleTest {
 
     @Test fun area() {
         assertEquals(24.0, Rectangle(4.0, 6.0).area())
+        assertEquals(24.0, Rectangle(4, 6).area())
+        assertEquals(24.0, Rectangle(4, 6).area)
+        assertEquals(25.0, Rectangle.square(5).area())
+    }
+
+    @Test fun perimeter() {
+        assertEquals(20.0, Rectangle(4.0, 6.0).perimeter())
+        assertEquals(20.0, Rectangle(4, 6).perimeter())
+        assertEquals(20.0, Rectangle(4, 6).perimeter)
+        assertEquals(20.0, Rectangle.square(5.0).perimeter())
+    }
+
+    @Test fun `range checks`() {
+        assertThrows<IllegalArgumentException> { Rectangle(0, 6.0) }
+        assertThrows<IllegalArgumentException> { Rectangle(4.0, 0) }
+        assertThrows<IllegalArgumentException> { Rectangle.square(0) }
     }
 }
