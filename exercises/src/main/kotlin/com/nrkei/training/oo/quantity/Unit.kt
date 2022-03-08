49cce61aa1400ca2d32c951ff368350276a6206d
Fred George <fredgeorge@acm.org>
2022-03-08 14:40:48 +0100

    Fred: Quantity for volume and distance that handles incompatible Units

--- exercises/src/main/kotlin/com/nrkei/training/oo/quantity/Unit.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/quantity/Unit.kt
@@ -43,18 +43,26 @@ class Unit {
         val Number.miles get() = Quantity(this, MILE)
         val Number.leagues get() = Quantity(this, LEAGUE)
     }
+
+    private val baseUnit: Unit
     private val baseUnitRatio: Double
 
     private constructor() {
+        baseUnit = this
         baseUnitRatio = 1.0
     }
 
     private constructor(relativeRatio: Number, relativeUnit: Unit) {
+        baseUnit = relativeUnit.baseUnit
         baseUnitRatio = relativeRatio.toDouble() * relativeUnit.baseUnitRatio
     }
 
     internal fun convertedAmount(otherAmount: Double, other: Unit) =
-        otherAmount * other.baseUnitRatio / this.baseUnitRatio
+        (otherAmount * other.baseUnitRatio / this.baseUnitRatio).also {
+            require(this isCompatible other)
+        }
 
     internal fun hashCode(amount: Double) = (amount * baseUnitRatio).hashCode()
+
+    internal infix fun isCompatible(other: Unit) = this.baseUnit == other.baseUnit
 }
